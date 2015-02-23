46c2f3a91f736c7463f1d43a632e2d5813ec05dc
lclc98 <liam.c14785@gmail.com>
2015-02-23 17:41:25 +1000

    Added GUI to ItemColorWand Switched Colour to Color

--- src/main/java/net/darkhax/colourfulmobs/common/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package net.darkhax.colourfulmobs.common;
-
-public class CommonProxy {
-
-    public void registerSidedEvents() {
-
-    }
-}
--- src/main/java/net/darkhax/colourfulmobs/common/ConfigurationHandler.java
+++
@@ -1,9 +0,0 @@
-package net.darkhax.colourfulmobs.common;
-
-public class ConfigurationHandler {
-
-    public static boolean spawnRandom = true;
-    public static boolean dropPowder = true;
-    public static boolean limitMobs = false;
-    public static String[] validMobs = null;
-}
