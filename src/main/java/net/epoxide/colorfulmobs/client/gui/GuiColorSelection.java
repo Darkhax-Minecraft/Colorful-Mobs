47bf3feebe878f104b076650d081ad8965ab5528
lclc98 <liam.c14785@gmail.com>
2015-03-10 15:21:35 +1000

    Added slider instead of text fields

--- src/main/java/net/epoxide/colorfulmobs/client/gui/GuiColorSelection.java
+++ src/main/java/net/epoxide/colorfulmobs/client/gui/GuiColorSelection.java
@@ -8,7 +8,6 @@ import net.epoxide.colorfulmobs.common.PacketColorSync;
 import net.epoxide.colorfulmobs.lib.Constants;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.entity.EntityList;
@@ -22,18 +21,18 @@ import org.lwjgl.opengl.GL12;
 
 public class GuiColorSelection extends GuiScreen {
 
-    private GuiTextField textR, textG, textB, textA;
+    private GuiSlider sliderR, sliderG, sliderB, sliderA;
     protected final int xSize = 177;
     protected final int ySize = 222;
 
     private EntityLivingBase entity;
     private EntityLivingBase tempEntity;
-    private int r = 255, g = 255, b = 255, a = 255;
+    private int r = 255, g = 255, b = 255, a = 100;
 
     public GuiColorSelection(EntityLivingBase entity) {
 
         this.entity = entity;
-        this.tempEntity = (EntityLivingBase) EntityList.createEntityByName(entity.getCommandSenderName(), (World) null);
+        this.tempEntity = (EntityLivingBase) EntityList.createEntityByName(EntityList.getEntityString(entity), (World) null);
         this.tempEntity.setWorld(entity.worldObj);
     }
 
@@ -47,10 +46,10 @@ public class GuiColorSelection extends GuiScreen {
 
         GL11.glColor4f(0.0F, 0.0F, 0.0F, 1.0F);
 
-        this.drawTexturedModalRect(k + 32, l + 30, 20, 20, 110, 115);
+        this.drawTexturedModalRect(k + 32, l + 20, 20, 20, 110, 115);
 
         if (tempEntity != null)
-            drawEntityOnScreen(k + 87, l + 130, 40, tempEntity);
+            drawEntityOnScreen(k + 87, l + 120, 40, tempEntity);
     }
 
     @Override
@@ -69,28 +68,21 @@ public class GuiColorSelection extends GuiScreen {
             r = (int) (obj.red * 255);
             g = (int) (obj.green * 255);
             b = (int) (obj.blue * 255);
-            a = (int) (obj.alpha * 255);
+            a = (int) (obj.alpha * 100);
+
+            ColorProperties.setEntityColors(obj, tempEntity);
         }
+        this.sliderR = new GuiSlider(2, k + 3, l + 140, 0, 255, r, EnumChatFormatting.RED + StatCollector.translateToLocal("chat.colorfulmobs.red"), this);
+        buttonList.add(this.sliderR);
 
-        this.textR = new GuiTextField(fontRendererObj, k + 17, l + 160, 30, 10);
-        this.textR.setMaxStringLength(3);
-        this.textR.setTextColor(16777215);
-        this.textR.setText(String.valueOf(r));
+        this.sliderG = new GuiSlider(3, k + 60, l + 140, 0, 255, g, EnumChatFormatting.GREEN + StatCollector.translateToLocal("chat.colorfulmobs.green"), this);
+        buttonList.add(this.sliderG);
 
-        this.textG = new GuiTextField(fontRendererObj, k + 54, l + 160, 30, 10);
-        this.textG.setMaxStringLength(3);
-        this.textG.setTextColor(16777215);
-        this.textG.setText(String.valueOf(g));
+        this.sliderB = new GuiSlider(4, k + 117, l + 140, 0, 255, b, EnumChatFormatting.BLUE + StatCollector.translateToLocal("chat.colorfulmobs.blue"), this);
+        buttonList.add(this.sliderB);
 
-        this.textB = new GuiTextField(fontRendererObj, k + 91, l + 160, 30, 10);
-        this.textB.setMaxStringLength(3);
-        this.textB.setTextColor(16777215);
-        this.textB.setText(String.valueOf(b));
-
-        this.textA = new GuiTextField(fontRendererObj, k + 128, l + 160, 30, 10);
-        this.textA.setMaxStringLength(3);
-        this.textA.setTextColor(16777215);
-        this.textA.setText(String.valueOf(a));
+        this.sliderA = new GuiSlider(5, k + 61, l + 165, 0, 100, a, EnumChatFormatting.WHITE + StatCollector.translateToLocal("chat.colorfulmobs.alpha"), this);
+        buttonList.add(this.sliderA);
     }
 
     @Override
@@ -101,20 +93,12 @@ public class GuiColorSelection extends GuiScreen {
 
         int k = (this.width - this.xSize) / 2;
         int l = (this.height - this.ySize) / 2;
-        drawString(this.fontRendererObj, EnumChatFormatting.RED + StatCollector.translateToLocal("chat.colorfulmobs.red"), k + 21, l + 150, 0xffffff);
-        drawString(this.fontRendererObj, EnumChatFormatting.GREEN + StatCollector.translateToLocal("chat.colorfulmobs.green"), k + 54, l + 150, 0xffffff);
-        drawString(this.fontRendererObj, EnumChatFormatting.BLUE + StatCollector.translateToLocal("chat.colorfulmobs.blue"), k + 96, l + 150, 0xffffff);
-        drawString(this.fontRendererObj, StatCollector.translateToLocal("chat.colorfulmobs.alpha"), k + 129, l + 150, 0xffffff);
 
-        drawCenteredString(fontRendererObj, StatCollector.translateToLocal("chat.colorfulmobs.colormenu"), k + 87, l + 15, 0xffffff);
+        drawCenteredString(fontRendererObj, StatCollector.translateToLocal("chat.colorfulmobs.colormenu"), k + 87, l + 10, 0xffffff);
 
-        this.textR.drawTextBox();
-        this.textG.drawTextBox();
-        this.textB.drawTextBox();
-        this.textA.drawTextBox();
     }
 
-    float zz = 0;
+    float rotation = 0;
 
     public void drawEntityOnScreen(int x, int y, float scale, EntityLivingBase entity) {
         GL11.glDisable(GL11.GL_BLEND);
@@ -127,7 +111,7 @@ public class GuiColorSelection extends GuiScreen {
         GL11.glTranslatef(x, y, 200);
         GL11.glScalef((-scale), scale, scale);
         GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-        GL11.glRotatef(zz, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(rotation, 0.0F, 1.0F, 0.0F);
         EntityUtil.renderEntityWithPosYaw(entity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F);
         GL11.glPopMatrix();
         RenderHelper.disableStandardItemLighting();
@@ -136,7 +120,7 @@ public class GuiColorSelection extends GuiScreen {
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
 
-        zz -= 1.25F;
+        rotation -= 1.25F;
     }
 
     @Override
@@ -147,43 +131,19 @@ public class GuiColorSelection extends GuiScreen {
             if (entity != null) {
 
                 // send packet
-                updateColor();
                 PacketColorSync packetColorSync = new PacketColorSync(new ColorObject(r, g, b, a), entity);
                 ColorfulMobs.network.sendToServer(packetColorSync);
             }
-
             // close gui
             this.mc.displayGuiScreen(null);
             this.mc.setIngameFocus();
+        } else {
+
+            if (button instanceof GuiSlider) {
+
+                updateColor();
+            }
         }
-    }
-
-    @Override
-    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
-
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-        textR.mouseClicked(mouseX, mouseY, mouseButton);
-        textG.mouseClicked(mouseX, mouseY, mouseButton);
-        textB.mouseClicked(mouseX, mouseY, mouseButton);
-        textA.mouseClicked(mouseX, mouseY, mouseButton);
-    }
-
-    @Override
-    protected void keyTyped(char typedChar, int keyCode) {
-
-        super.keyTyped(typedChar, keyCode);
-
-        if (textR.isFocused())
-            addText(textR, typedChar, keyCode);
-
-        else if (textG.isFocused())
-            addText(textG, typedChar, keyCode);
-
-        else if (textB.isFocused())
-            addText(textB, typedChar, keyCode);
-
-        else if (textA.isFocused())
-            addText(textA, typedChar, keyCode);
     }
 
     /**
@@ -191,132 +151,19 @@ public class GuiColorSelection extends GuiScreen {
      */
     public void updateColor() {
 
-        String color = textR.getText();
-        if (color != null && !color.equals(""))
-            r = Integer.parseInt(color);
+        float color = sliderR.getValue();
+        r = (int) sliderG.denormalizeValue(color);
 
-        color = textG.getText();
-        if (color != null && !color.equals(""))
-            g = Integer.parseInt(color);
+        color = sliderG.getValue();
+        g = (int) sliderG.denormalizeValue(color);
 
-        color = textB.getText();
-        if (color != null && !color.equals(""))
-            b = Integer.parseInt(color);
+        color = sliderB.getValue();
+        b = (int) sliderB.denormalizeValue(color);
 
-        color = textA.getText();
-        if (color != null && !color.equals(""))
-            a = Integer.parseInt(color);
+        color = sliderA.getValue();
+        a = (int) sliderA.denormalizeValue(color);
 
         ColorProperties.setEntityColors(new ColorObject(r, g, b, a), tempEntity);
-
     }
 
-    /**
-     * Adds a character to a textField. Used for typing the RGBA values.
-     *
-     * @param textField: The GUITextField being added to.
-     * @param typedChar: The character which is being added to the text field.
-     * @param keyCode:   A numeric value which represents the key being typed.
-     * @return boolean: Will return true if the character was successfully added.
-     */
-    private boolean addText(GuiTextField textField, char typedChar, int keyCode) {
-
-        if (typedChar >= '0' && typedChar <= '9') {
-
-            textField.textboxKeyTyped(typedChar, keyCode);
-
-            if (Integer.parseInt(textField.getText()) <= 255) {
-
-                updateColor();
-                return true;
-            } else {
-
-                textField.setText("255");
-                updateColor();
-                return true;
-            }
-        } else {
-
-            switch (keyCode) {
-
-                case 14:
-                    if (GuiScreen.isCtrlKeyDown()) {
-
-                        textField.deleteWords(-1);
-                        updateColor();
-                    } else {
-
-                        textField.deleteFromCursor(-1);
-                        updateColor();
-                    }
-
-                    return true;
-
-                case 199:
-                    if (GuiScreen.isShiftKeyDown())
-                        textField.setSelectionPos(0);
-
-                    else
-                        textField.setCursorPositionZero();
-
-                    return true;
-
-                case 203:
-                    if (GuiScreen.isShiftKeyDown()) {
-
-                        if (GuiScreen.isCtrlKeyDown())
-                            textField.setSelectionPos(textField.getNthWordFromPos(-1, textField.getSelectionEnd()));
-
-                        else
-                            textField.setSelectionPos(textField.getSelectionEnd() - 1);
-                    } else if (GuiScreen.isCtrlKeyDown())
-                        textField.setCursorPosition(textField.getNthWordFromCursor(-1));
-
-                    else
-                        textField.moveCursorBy(-1);
-
-                    return true;
-
-                case 205:
-                    if (GuiScreen.isShiftKeyDown()) {
-
-                        if (GuiScreen.isCtrlKeyDown())
-                            textField.setSelectionPos(textField.getNthWordFromPos(1, textField.getSelectionEnd()));
-
-                        else
-                            textField.setSelectionPos(textField.getSelectionEnd() + 1);
-                    } else if (GuiScreen.isCtrlKeyDown())
-                        textField.setCursorPosition(textField.getNthWordFromCursor(1));
-
-                    else
-                        textField.moveCursorBy(1);
-
-                    return true;
-
-                case 207:
-                    if (GuiScreen.isShiftKeyDown())
-                        textField.setSelectionPos(textField.getText().length());
-
-                    else
-                        textField.setCursorPositionEnd();
-
-                    return true;
-
-                case 211:
-                    if (GuiScreen.isCtrlKeyDown()) {
-
-                        textField.deleteWords(1);
-                        updateColor();
-                    } else {
-
-                        textField.deleteFromCursor(1);
-                        updateColor();
-                    }
-
-                    return true;
-            }
-        }
-
-        return false;
-    }
 }
---
+++ src/main/java/net/epoxide/colorfulmobs/client/gui/GuiSlider.java
@@ -0,0 +1,120 @@
+package net.epoxide.colorfulmobs.client.gui;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.util.MathHelper;
+import org.lwjgl.opengl.GL11;
+
+@SideOnly(Side.CLIENT)
+public class GuiSlider extends GuiButton {
+    private float currentValue;
+    public boolean field_146135_o;
+    private final float valueMin;
+    private final float valueMax;
+    private final String color;
+    private final float valueStep = 1;
+    private GuiColorSelection gui;
+
+    public GuiSlider(int id, int xPos, int yPos, float valueMin, float valueMax, float currentValue, String color, GuiColorSelection gui) {
+        super(id, xPos, yPos, 55, 20, "");
+        this.currentValue = 1.0F;
+        this.valueMin = valueMin;
+        this.valueMax = valueMax;
+        this.currentValue = normalizeValue(currentValue);
+        this.color = color;
+        this.displayString = color + ": " + (int) currentValue;
+
+        this.gui = gui;
+    }
+
+    @Override
+    public int getHoverState(boolean p_146114_1_) {
+        return 0;
+    }
+
+    @Override
+    protected void mouseDragged(Minecraft p_146119_1_, int p_146119_2_, int p_146119_3_) {
+        if (this.visible) {
+            if (this.field_146135_o) {
+                this.currentValue = (float) (p_146119_2_ - (this.xPosition + 4)) / (float) (this.width - 8);
+
+                if (this.currentValue < 0.0F) {
+                    this.currentValue = 0.0F;
+                }
+
+                if (this.currentValue > 1.0F) {
+                    this.currentValue = 1.0F;
+                }
+
+                float f = denormalizeValue(this.currentValue);
+                currentValue = f;
+                this.currentValue = normalizeValue(f);
+                this.displayString = color + ": " + (int) f;
+
+                gui.updateColor();
+            }
+
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            this.drawTexturedModalRect(this.xPosition + (int) (this.currentValue * (float) (this.width - 8)), this.yPosition, 0, 66, 4, 20);
+            this.drawTexturedModalRect(this.xPosition + (int) (this.currentValue * (float) (this.width - 8)) + 4, this.yPosition, 196, 66, 4, 20);
+        }
+    }
+
+    @Override
+    public boolean mousePressed(Minecraft p_146116_1_, int p_146116_2_, int p_146116_3_) {
+        if (super.mousePressed(p_146116_1_, p_146116_2_, p_146116_3_)) {
+            this.currentValue = (float) (p_146116_2_ - (this.xPosition + 4)) / (float) (this.width - 8);
+
+            if (this.currentValue < 0.0F) {
+                this.currentValue = 0.0F;
+            }
+
+            if (this.currentValue > 1.0F) {
+                this.currentValue = 1.0F;
+            }
+            this.displayString = color + ": " + (int) denormalizeValue(this.currentValue);
+            this.field_146135_o = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void mouseReleased(int p_146118_1_, int p_146118_2_) {
+        this.field_146135_o = false;
+        gui.updateColor();
+    }
+
+    public void setValue(float value) {
+        this.currentValue += value;
+    }
+
+    public float getValue() {
+        return this.currentValue;
+    }
+
+
+    public float normalizeValue(float value) {
+        return MathHelper.clamp_float((this.snapToStepClamp(value) - this.valueMin) / (this.valueMax - this.valueMin), 0.0F, 1.0F);
+    }
+
+    public float denormalizeValue(float value) {
+        return this.snapToStepClamp(this.valueMin + (this.valueMax - this.valueMin) * MathHelper.clamp_float(value, 0.0F, 1.0F));
+    }
+
+    private float snapToStepClamp(float value) {
+        value = this.snapToStep(value);
+        return MathHelper.clamp_float(value, this.valueMin, this.valueMax);
+    }
+
+    private float snapToStep(float value) {
+        if (this.valueStep > 0.0F) {
+            value = this.valueStep * (float) Math.round(value / this.valueStep);
+        }
+
+        return value;
+    }
+}
