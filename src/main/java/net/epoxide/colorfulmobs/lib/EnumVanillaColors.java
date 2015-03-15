7e5ba6fb1ab29e0648afbaa621937ea956e8966a
Darkhax <darklime@live.ca>
2015-03-15 00:18:52 -0600

    Switched names to oreDict names.

--- src/main/java/net/epoxide/colorfulmobs/lib/EnumVanillaColors.java
+++ src/main/java/net/epoxide/colorfulmobs/lib/EnumVanillaColors.java
@@ -2,22 +2,22 @@ package net.epoxide.colorfulmobs.lib;
 
 public enum EnumVanillaColors {
 
-    BLACK("black", new ColorObject(25, 25, 25)),
-    RED("red", new ColorObject(153, 51, 51)),
-    GREEN("green", new ColorObject(102, 127, 51)),
-    BROWN("brown", new ColorObject(102, 76, 51)),
-    BLUE("blue", new ColorObject(51, 76, 178)),
-    PURPLE("purple", new ColorObject(127, 63, 178)),
-    CYAN("cyan", new ColorObject(76, 127, 153)),
-    LIGHT_GRAY("lightGray", new ColorObject(153, 153, 153)),
-    GRAY("gray", new ColorObject(76, 76, 76)),
-    PINK("pink", new ColorObject(242, 127, 165)),
-    LIME("lime", new ColorObject(127, 204, 25)),
-    YELLOW("yellow", new ColorObject(229, 229, 51)),
-    LIGHT_BLUE("lightBlue", new ColorObject(102, 153, 216)),
-    MAGENTAG("magenta", new ColorObject(178, 76, 216)),
-    ORANGE("orange", new ColorObject(216, 127, 5)),
-    WHITE("white", new ColorObject(255, 255, 255));
+    BLACK("dyeBlack", new ColorObject(25, 25, 25)),
+    RED("dyeRed", new ColorObject(153, 51, 51)),
+    GREEN("dyeGreen", new ColorObject(102, 127, 51)),
+    BROWN("dyeBrown", new ColorObject(102, 76, 51)),
+    BLUE("dyeBlue", new ColorObject(51, 76, 178)),
+    PURPLE("dyePurple", new ColorObject(127, 63, 178)),
+    CYAN("dyeCyan", new ColorObject(76, 127, 153)),
+    LIGHT_GRAY("dyeLightGray", new ColorObject(153, 153, 153)),
+    GRAY("dyeGray", new ColorObject(76, 76, 76)),
+    PINK("dyePink", new ColorObject(242, 127, 165)),
+    LIME("dyeLime", new ColorObject(127, 204, 25)),
+    YELLOW("dyeYellow", new ColorObject(229, 229, 51)),
+    LIGHT_BLUE("dyeLightBlue", new ColorObject(102, 153, 216)),
+    MAGENTAG("dyeMagenta", new ColorObject(178, 76, 216)),
+    ORANGE("dyeOrange", new ColorObject(216, 127, 5)),
+    WHITE("dyeWhite", new ColorObject(255, 255, 255));
 
     public String colorName;
     public ColorObject colorObj;
