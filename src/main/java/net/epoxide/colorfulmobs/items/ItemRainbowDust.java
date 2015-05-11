b7fc0efdb97183d865b37ccbb86207353441bdb9
Darkhax <darklime@live.ca>
2015-05-10 18:48:15 -0600

    Added a tooltip

--- src/main/java/net/epoxide/colorfulmobs/items/ItemRainbowDust.java
+++ src/main/java/net/epoxide/colorfulmobs/items/ItemRainbowDust.java
@@ -1,16 +1,20 @@
 package net.epoxide.colorfulmobs.items;
 
+import java.util.List;
+
 import net.epoxide.colorfulmobs.handler.AchievementHandler;
 import net.epoxide.colorfulmobs.lib.ColorObject;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemRainbowDust extends ItemColorSetter {
     
     public ItemRainbowDust() {
     
-        this.hasSubtypes = true;
         this.setTextureName("colorfulmobs:rainbowdust");
         this.setUnlocalizedName("colorfulmobs.rainbowdust");
     }
@@ -33,5 +37,12 @@ public class ItemRainbowDust extends ItemColorSetter {
         super.itemInteractionForEntity(stack, player, entity);
         player.triggerAchievement(AchievementHandler.achRainbow);
         return true;
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation (ItemStack stack, EntityPlayer player, List list, boolean advanced) {
+    
+        list.add(StatCollector.translateToLocal("tooltip.colorfulmobs.rainbow"));
     }
 }
