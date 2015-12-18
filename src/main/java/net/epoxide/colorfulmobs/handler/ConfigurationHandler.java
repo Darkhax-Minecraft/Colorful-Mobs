3101750c0bfd6b8b80a4b3736b39d4dee325f620
Darkhax <darklime@live.ca>
2015-12-18 07:41:24 -0700

    Cleaned up the configuraton

--- src/main/java/net/epoxide/colorfulmobs/handler/ConfigurationHandler.java
+++ src/main/java/net/epoxide/colorfulmobs/handler/ConfigurationHandler.java
@@ -15,10 +15,10 @@ import net.epoxide.colorfulmobs.lib.Constants;
 public class ConfigurationHandler {
     
     public static boolean spawnRandom = true;
-    public static double spawnRate = 0.04d;
+    public static float spawnRate = 0.04f;
     public static boolean dropPowder = true;
     public static boolean limitMobs = false;
-    public static List<String> validMobs = null;
+    public static String[] validMobs = new String[] {};
     public static boolean dyePlayer = false;
     public static boolean craftDye = true;
     public static boolean cloneDye = true;
@@ -27,9 +27,7 @@ public class ConfigurationHandler {
     public static boolean craftRainbowWand = true;
     public static boolean findRainbowDust = true;
     public static int rainbowDustRate = 10;
-    public static List<String> validLootLocations = null;
-    
-    public static final String GENERAL = "general";
+    public static String[] validLootLocations = new String[] {"dungeonChest", "pyramidJungleDispenser", "strongholdLibrary", "pyramidDesertyChest", "bonusChest"};
     
     public static Configuration config;
     
@@ -50,88 +48,20 @@ public class ConfigurationHandler {
     
     private void syncConfigData () {
         
-        List<String> propOrder = new ArrayList<String>();
-        Property prop;
-        
-        prop = config.get(GENERAL, "Spawned Colored Mobs", true);
-        prop.comment = "Should mobs randomly be colored when they spawn?";
-        prop.setLanguageKey("colorfulmobs.configGui.spawnRandom");
-        spawnRandom = prop.getBoolean(true);
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Dyed Spawn Chance", 0.04);
-        prop.comment = "What percent chance should mobs spawn randomly colored?";
-        prop.setLanguageKey("colorfulmobs.configGui.spawnRate");
-        spawnRate = prop.getDouble();
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Drop Dye Powder", true);
-        prop.comment = "Should mobs drop dye powder on death?";
-        prop.setLanguageKey("colorfulmobs.configGui.dropPowder");
-        dropPowder = prop.getBoolean(true);
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Dyeable Players", false);
-        prop.comment = "Should players be able to dye each other?";
-        prop.setLanguageKey("colorfulmobs.configGui.playerDye");
-        dyePlayer = prop.getBoolean(false);
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Limit To Valid Mobs", false);
-        prop.comment = "Should only mobs on the valid list be able to spawn with random colors?";
-        prop.setLanguageKey("colorfulmobs.configGui.limitMobs");
-        limitMobs = prop.getBoolean(false);
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Craft Blank Dye", true);
-        prop.comment = "Should blank dye powder be craftable?";
-        prop.setLanguageKey("colorfulmobs.configGui.craftDye");
-        craftDye = prop.getBoolean(true);
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Craft Data Checker", true);
-        prop.comment = "Should the Data Checker be craftable?";
-        prop.setLanguageKey("colorfulmobs.configGui.dataChecker");
-        cloneDye = prop.getBoolean(true);
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Clone Dyes", true);
-        prop.comment = "Should players be able to copy their dye colors?";
-        prop.setLanguageKey("colorfulmobs.configGui.cloneDye");
-        craftBook = prop.getBoolean(true);
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Craft Ghost Dust", true);
-        prop.comment = "Should players be able to craft ghost dust?";
-        prop.setLanguageKey("colorfulmobs.configGui.ghostDust");
-        craftGhostDust = prop.getBoolean(true);
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Can Find Rainbow Dust", true);
-        prop.comment = "Can rainbow dust be found in loot chests?";
-        prop.setLanguageKey("colorfulmobs.configGui.canFindRainbow");
-        findRainbowDust = prop.getBoolean();
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Can Craft Rainbow Wand", true);
-        prop.comment = "Should the rainbow wand be craftable?";
-        prop.setLanguageKey("colorfulmobs.configGui.craftWand");
-        craftRainbowWand = prop.getBoolean();
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Rainbow Dust Spawn Rate", 10);
-        prop.comment = "How often should rainbow dust be found in chests?";
-        prop.setLanguageKey("colorfulmobs.configGui.rainbowRate");
-        rainbowDustRate = prop.getInt();
-        propOrder.add(prop.getName());
-        
-        prop = config.get(GENERAL, "Valid Chest List", new String[] { "dungeonChest", "pyramidJungleDispenser", "strongholdLibrary", "pyramidDesertyChest", "bonusChest" });
-        prop.comment = "A list of valid chest types for chest loot to show up in.";
-        prop.setLanguageKey("colorfulmobs.configGui.chestLoot");
-        validLootLocations = Arrays.asList(prop.getStringList());
-        propOrder.add(prop.getName());
-        
-        config.setCategoryPropertyOrder(GENERAL, propOrder);
+        String category = "Settings";
+        spawnRandom = config.getBoolean("spawnRandom", category, spawnRandom, "Should mobs randomly spawn with a color?");
+        spawnRate = config.getFloat("spawnRate", category, spawnRate, 0.0f, 1.0f, "What percentage of mobs should spawn with a random color? 0.0f is 0%, 0.55f is 55% and 1.0 is 100%");
+        dropPowder = config.getBoolean("dropPowder", category, dropPowder, "Should a mob drop their powder on death?");
+        limitMobs = config.getBoolean("limitMobs", category, limitMobs, "Should only mobs on the list of valid mobs spawn with a random color?");
+        craftDye = config.getBoolean("craftDyes", category, craftDye, "Should vanilla dye colors be craftable?");
+        cloneDye = config.getBoolean("cloneDye", category, cloneDye, "Should players be able to copy a dye by mixing it with a pure white dye?");
+        craftBook = config.getBoolean("craftBook", category, craftBook, "Should players be able to craft the data checker book?");
+        craftGhostDust = config.getBoolean("craftGhostDust", category, craftGhostDust, "Should players be able to craft the ghost dust?");
+        findRainbowDust = config.getBoolean("findRainbowDust", category, findRainbowDust, "Should players be able to find rainbow dusts in chests?");
+        craftRainbowWand = config.getBoolean("craftRainbowWand", category, craftRainbowWand, "Should Rainbow Wands be craftable?");
+        rainbowDustRate = config.getInt("rainbowDustWeight", category, rainbowDustRate, 0, 100, "How often should the rainbow dust show up in dungeon chests? Higher means more often.");
+        validLootLocations = config.getStringList("validLootLocations", category, validLootLocations, "A list of valid loot locations for the rainbow dust and other loot to spawn in.");
+        validMobs = config.getStringList("validMobs", category, validMobs, "A list of mobs that CAN spawn with a color effect on them. This is only requred is limitMobs is true.");
         
         if (config.hasChanged())
             config.save();
