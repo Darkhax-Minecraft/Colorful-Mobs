26436faca1bb14d9c4ba2bcc9b1fe5a91163fbff
Darkhax <darklime@live.ca>
2015-12-26 15:57:57 -0700

    Re-introduced the creative tab.

--- src/main/java/net/epoxide/colorfulmobs/ColorfulMobs.java
+++ src/main/java/net/epoxide/colorfulmobs/ColorfulMobs.java
@@ -1,5 +1,7 @@
 package net.epoxide.colorfulmobs;
 
+import net.minecraft.creativetab.CreativeTabs;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
@@ -8,18 +10,20 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
 import net.epoxide.colorfulmobs.common.CommonProxy;
+import net.epoxide.colorfulmobs.creativetab.CreativeTabColorfulMobs;
 import net.epoxide.colorfulmobs.lib.Constants;
 
 @Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, guiFactory = Constants.FACTORY, dependencies = Constants.DEPENDANCIES)
 public class ColorfulMobs {
-    
-    public static SimpleNetworkWrapper network;
     
     @SidedProxy(clientSide = Constants.CLIENT_PROXY_CLASS, serverSide = Constants.SERVER_PROXY_CLASS)
     public static CommonProxy proxy;
     
     @Mod.Instance(Constants.MOD_ID)
     public static ColorfulMobs instance;
+    
+    public static SimpleNetworkWrapper network;
+    public static CreativeTabs tabColors = new CreativeTabColorfulMobs();
     
     @EventHandler
     public void preInit (FMLPreInitializationEvent pre) {
