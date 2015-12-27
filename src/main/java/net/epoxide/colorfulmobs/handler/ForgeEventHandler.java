8ad446db6d61ffe20ee263938efdb7a9c7eae116
Darkhax <darklime@live.ca>
2015-12-27 16:07:49 -0700

    Re-Added the ColorProperties to the entity construction.

--- src/main/java/net/epoxide/colorfulmobs/handler/ForgeEventHandler.java
+++ src/main/java/net/epoxide/colorfulmobs/handler/ForgeEventHandler.java
@@ -1,5 +1,20 @@
 package net.epoxide.colorfulmobs.handler;
 
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+
+import net.minecraftforge.event.entity.EntityEvent;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+import net.epoxide.colorfulmobs.common.ColorProperties;
+
 public class ForgeEventHandler {
 
+    @SubscribeEvent
+    public void onEntityConstructed (EntityEvent.EntityConstructing event) {
+    
+        if (event.entity instanceof EntityLiving && ColorProperties.hasProperties((EntityLivingBase) event.entity))
+            ColorProperties.setProperties((EntityLivingBase) event.entity);
+    }
 }
