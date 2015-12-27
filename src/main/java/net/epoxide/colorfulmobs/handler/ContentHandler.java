cd7e71057612a565a90e83a47603fc8a653fd82f
Darkhax <darklime@live.ca>
2015-12-27 16:27:50 -0700

    Created a ContentHandler

---
+++ src/main/java/net/epoxide/colorfulmobs/handler/ContentHandler.java
@@ -0,0 +1,22 @@
+package net.epoxide.colorfulmobs.handler;
+
+import net.minecraft.item.Item;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+import net.epoxide.colorfulmobs.item.ItemRGBDust;
+
+public class ContentHandler {
+    
+    public static Item itemRGBDust;
+    public static Item itemAlphaDust;
+    public static Item itemRainbowDust;
+    public static Item itemRainbowWand;
+    public static Item itemDataChecker;
+    
+    public static void initItems () {
+        
+        itemRGBDust = new ItemRGBDust();
+        GameRegistry.registerItem(itemRGBDust, "rgb_dust");
+    }
+}
