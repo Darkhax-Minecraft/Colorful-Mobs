6eb3a3ab6f8dc476d6d7f55e04518a5b036e6c27
Darkhax <darklime@live.ca>
2015-12-28 13:59:05 -0700

    Fixed render state issue when mob is not dyed.

--- src/main/java/net/epoxide/colorfulmobs/client/RenderingHandler.java
+++ src/main/java/net/epoxide/colorfulmobs/client/RenderingHandler.java
@@ -2,6 +2,8 @@ package net.epoxide.colorfulmobs.client;
 
 import org.lwjgl.opengl.GL11;
 
+import net.minecraft.entity.EntityLivingBase;
+
 import net.minecraftforge.client.event.RenderLivingEvent;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -15,16 +17,21 @@ public class RenderingHandler {
     @SubscribeEvent
     public void onEntityRenderPre (RenderLivingEvent.Pre event) {
         
-        if (event.entity != null) {
+        if (event.entity instanceof EntityLivingBase) {
             
             if (ColorProperties.hasProperties(event.entity)) {
                 
-                ColorObject obj = ColorProperties.getProperties(event.entity).getColorObj();
+                ColorProperties props = ColorProperties.getProperties(event.entity);
                 
-                GL11.glPushMatrix();
-                GL11.glEnable(GL11.GL_BLEND);
-                GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
-                GL11.glColor4f(obj.getRed(), obj.getGreen(), obj.getBlue(), obj.getAlpha());
+                if (props.isDyed()) {
+                    
+                    ColorObject obj = props.getColorObj();
+                    
+                    GL11.glPushMatrix();
+                    GL11.glEnable(GL11.GL_BLEND);
+                    GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
+                    GL11.glColor4f(obj.getRed(), obj.getGreen(), obj.getBlue(), obj.getAlpha());
+                }
             }
         }
     }
@@ -32,10 +39,18 @@ public class RenderingHandler {
     @SubscribeEvent
     public void onEntityRenderPost (RenderLivingEvent.Post event) {
         
-        if (ColorProperties.hasProperties(event.entity)) {
+        if (event.entity instanceof EntityLivingBase) {
             
-            GL11.glDisable(GL11.GL_BLEND);
-            GL11.glPopMatrix();
+            if (ColorProperties.hasProperties(event.entity)) {
+                
+                ColorProperties props = ColorProperties.getProperties(event.entity);
+                
+                if (props.isDyed()) {
+                    
+                    GL11.glDisable(GL11.GL_BLEND);
+                    GL11.glPopMatrix();
+                }
+            }
         }
     }
 }
