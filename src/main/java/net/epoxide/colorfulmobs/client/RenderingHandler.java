c356d4618ec406e8b4de74e86b95d583742f69e5
Darkhax <darklime@live.ca>
2015-12-27 17:50:20 -0700

    Re-added the rendering effect.

---
+++ src/main/java/net/epoxide/colorfulmobs/client/RenderingHandler.java
@@ -0,0 +1,41 @@
+package net.epoxide.colorfulmobs.client;
+
+import org.lwjgl.opengl.GL11;
+
+import net.minecraftforge.client.event.RenderLivingEvent;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+import net.darkhax.bookshelf.lib.ColorObject;
+
+import net.epoxide.colorfulmobs.common.ColorProperties;
+
+public class RenderingHandler {
+    
+    @SubscribeEvent
+    public void onEntityRenderPre (RenderLivingEvent.Pre event) {
+        
+        if (event.entity != null) {
+            
+            if (ColorProperties.hasProperties(event.entity)) {
+                
+                ColorObject obj = ColorProperties.getProperties(event.entity).getColorObj();
+                
+                GL11.glPushMatrix();
+                GL11.glEnable(GL11.GL_BLEND);
+                GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
+                GL11.glColor4f(obj.getRed(), obj.getGreen(), obj.getBlue(), obj.getAlpha());
+            }
+        }
+    }
+    
+    @SubscribeEvent
+    public void onEntityRenderPost (RenderLivingEvent.Post event) {
+        
+        if (ColorProperties.hasProperties(event.entity)) {
+            
+            GL11.glDisable(GL11.GL_BLEND);
+            GL11.glPopMatrix();
+        }
+    }
+}
