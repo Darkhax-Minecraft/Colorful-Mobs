1f3ef9ae45dc8097c48b3947a69a8e14a0d18737
Darkhax <darklime@live.ca>
2015-12-27 17:36:26 -0700

    Re-added dungeon loot.

--- src/main/java/net/epoxide/colorfulmobs/handler/ContentHandler.java
+++ src/main/java/net/epoxide/colorfulmobs/handler/ContentHandler.java
@@ -1,6 +1,10 @@
 package net.epoxide.colorfulmobs.handler;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.WeightedRandomChestContent;
+
+import net.minecraftforge.common.ChestGenHooks;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -14,6 +18,9 @@ public class ContentHandler {
     public static Item itemRainbowWand;
     public static Item itemDataChecker;
     
+    /**
+     * Initializes all items within the mod.
+     */
     public static void initItems () {
         
         itemRGBDust = new ItemRGBDust();
@@ -30,5 +37,21 @@ public class ContentHandler {
         
         itemDataChecker = new ItemDataChecker();
         GameRegistry.registerItem(itemDataChecker, "data_checker");
+    }
+    
+    /**
+     * Initializes all the recipes in the mod.
+     */
+    public static void initRecipes() {
+        
+    }
+    
+    /**
+     * Initializes dungeon loot.
+     */
+    public static void initDungeonLoot() {
+        
+        for (String chestType : ConfigurationHandler.validLootLocations)
+            ChestGenHooks.addItem(chestType, new WeightedRandomChestContent(new ItemStack(ContentHandler.itemRainbowDust), 1, 1, ConfigurationHandler.rainbowDustRate));
     }
 }
