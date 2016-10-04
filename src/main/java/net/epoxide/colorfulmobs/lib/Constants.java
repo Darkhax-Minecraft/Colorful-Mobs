8edf034d09a315a4f317b2baa7e2e42e72f04bf5
Tyler Hancock <darklime@live.ca>
2016-10-04 03:00:04 -0600

    Fixed gradle. Probably.

--- src/main/java/net/epoxide/colorfulmobs/lib/Constants.java
+++ src/main/java/net/epoxide/colorfulmobs/lib/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final String VERSION_NUMBER = "2.0.0.0";
     public static final String CLIENT_PROXY_CLASS = "net.epoxide.colorfulmobs.client.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "net.epoxide.colorfulmobs.common.CommonProxy";
-    public static final String DEPENDENCIES = "required-after:bookshelf@[1.0.4.0,)";
+    public static final String DEPENDENCIES = "required-after:bookshelf@[1.4.1.320,)";
     public static final Random RANDOM = new Random();
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 }
